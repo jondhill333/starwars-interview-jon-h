53fbc4f2dee73695717ed82ab7de557221d3b537
Damian <dwood@uw.co.uk>
2022-11-08 15:27:04 +0000

    add optional operator

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ const TYPES = {
 
 export function doExtractYearFromBBY(str) {
   try {
-    if (typeof str !== TYPES.string) {
+    if (typeof str !== TYPES?.string) {
       console.error(`${str} was not a string`);
       throw Error("not a string");
     }
@@ -12,7 +12,7 @@ export function doExtractYearFromBBY(str) {
       console.error(`${str} did not end in BBY`);
       throw Error("not in BBY format");
     }
-    const number = parseInt(str.substring(0, 2));
+    const number = parseInt(str?.substring(0, 2));
     if (isNaN(number)) {
       console.error(`${str} was not a number`);
       throw Error("not a number");
