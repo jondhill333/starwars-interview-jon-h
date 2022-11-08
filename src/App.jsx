56351240064c69d5bc96ef12c567372ae7c43130
Damian <dwood@uw.co.uk>
2022-11-08 12:59:21 +0000

    list people

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+const App = () => {
+  const [people, setPeople] = useState()
+
+  useEffect(() => {
+    const req = new XMLHttpRequest();
+    try {
+      req.onload = () => {
+        const data = JSON.parse(req.responseText)
+        setPeople(data.results)
+      }
+    } catch (error) {
+      return <div>Something went wrong</div>
+    }
+    req.open("GET", "https://swapi.dev/api/people");
+    req.send();
+  }, [])
+
+  return (
+    <ul>
+      {people ? people.map(person => <Person person={person} />) : 'Loading'}
+    </ul>
+  );
+}
+
+class Person extends React.Component {
+  async componentDidMount() {
+    try {
+      const resp = await window.fetch(this.props.person['url'])
+      const text = await resp.text()
+      const person = JSON.parse(text)
+      this.setState({ person: person })
+    } catch {
+      return <b>Something went wrong</b>
+    }
+  }
+
+  render() {
+    const hasState = this.state !== null
+    if (hasState === false) {
+      return "Loading..."
+    } else if (hasState === true) {
+      return (
+        <div>
+          <h3>{this.state.person.name}</h3>
+          <div>{this.state.person.birth_year}</div>
+        </div>
+      )
+    } else
+      console.error('hasState is not a boolean')
+      return "Something went wrong"
+  }
+}
+
+export default App;
